c76fdcb18e7d0bae71d916e4ecf3d80d6f8d33cb
Yit Singh <Yitsingh751@hotmail.com>
2023-05-08 11:42:56 +0200

    Created new quote component

--- examensprojekt/src/App.js
+++ examensprojekt/src/App.js
@@ -1,13 +1,21 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import './App.css';
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Quotes from "./components/Quotes";
+import Quote from "./components/Quote";
+import "./App.css";
 
 function App() {
   return (
-    <div className="containerWrapper">
-      <Navbar/>
-      <p>hello</p>
+    <div>
+      <Navbar />
+      <div className="containerWrapper">
+        <Routes>
+          <Route path="/" element={<Quotes />} />
+          <Route path="/quote" element={<Quote name="kalle" />} />
+          <Route path="/quote/:id" element={<Quote />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- examensprojekt/src/components/Navbar.js
+++ examensprojekt/src/components/Navbar.js
@@ -1,26 +1,35 @@
-import React from "react"
+import { React, useRef } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav class="navbar">
-        <a href="#" class="toggle-button">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-        </a>
-        <div class="navbar-links">
+  const toggleRef = useRef();
+
+  const showNavbar = () => {
+    toggleRef.current.classList.toggle("active");
+  };
+
+  return (
+    <nav class="navbar">
+      <a href="#" class="toggle-button" onClick={showNavbar}>
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </a>
+      <div class="navbar-links" ref={toggleRef}>
         <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="recycle.html">Recycle</a></li>
-            <li><a href="contact.html">Contact us</a></li>
-            <li><a href="faq.html">FAQ</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="products.html">Products</a></li>
-            <li><a href="cart.html">Shopping cart</a></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/quote">My quotes</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
         </ul>
-        </div>
+      </div>
     </nav>
-    )
-}
+  );
+};
 
 export default Navbar;
